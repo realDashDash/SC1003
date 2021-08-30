2655970ca0f224ebcda40303a89f084593d5d045
Danxu Zhang <dzhang022@e.ntu.edu.sg>
2021-08-30 11:18:07 +0800

    Update Discussion 2
    Add special characters
    Add string build-in functions to check
    Update: enter again if password is not valid
    todo: regular expression operations

--- Week4/Discussion2.py
+++ Week4/Discussion2.py
@@ -2,29 +2,42 @@
 #    - more than 8 characters
 #    - at least one upper letter and lower letter
 #    - at least one number
+#    - at least one special character
+
+# todo regular expression operations
 
 def check_pw(password):
     length = 0
     upper = False
     lower = False
     number = False
+    special = False
+    notValid = False
+
+    Special_char = ['@', '#', '$', '^', '&', '*']
 
     length = len(password)
     for ch in password:
-        if (ord(ch) >= 97 and  ord(ch) <= 122):
+        if (ord(ch) >= 97 and  ord(ch) <= 122): # if (password.islower():)
             lower = True
-        if (ord(ch) >= 65 and  ord(ch) <= 90):
+        elif (ord(ch) >= 65 and  ord(ch) <= 90): # if (password.isupper():)
             upper = True
-        if (ord(ch) >= 48 and  ord(ch) <= 57):
+        elif (ord(ch) >= 48 and  ord(ch) <= 57): # if (password.isdigit():)
             number = True
-    if (length >= 8 and upper and lower and number):
+        elif (ch in Special_char): # if(not password.isalnum)
+            special = True
+        else:
+            notValid = True
+
+    if (length >= 8 and upper and lower and number and special and notValid):
         return True
     else:
         return False
 
-pw = input("Please enter password: ")
-if (check_pw(pw)):
-    print("Success!")
-else:
-    print("Please enter another password!")
-    
+while (True):
+    pw = input("Please enter password: ")
+    if (check_pw(pw)):
+        print("Success!")
+        break;
+    else:
+        print("Please enter another password!")
