9a799b11272ba821b8a7694d88b4cc94a2dcf04b
Danxu Zhang <dzhang022@e.ntu.edu.sg>
2021-09-06 11:28:47 +0800

    Update Lab5b.py

--- Week5/Lab5b.py
+++ Week5/Lab5b.py
@@ -10,48 +10,54 @@ R = (255, 0, 0)
 W = (255,255,255)
 O = (0,0,0)
 P = (255,105, 180)
-image_pixels =[B, B, B, B, B, B, B, B,
-               B, B, B, Y, B, B, B, B,
-               B, B, Y, Y, Y, B, B, B,
-               B, Y, B, Y, B, Y, B, B,
-               B, B, B, Y, B, B, B, B,
-               B, B, B, B, Y, B, B, B,
-               B, B, B, Y, B, B, B, B, 
-               B, B, B, B, B, B, B, G]
-raspi = [
-    O, G, G, O, O, G, G, O, 
-    O, O, G, G, G, G, O, O,
-    O, O, R, R, R, R, O, O, 
-    O, R, R, R, R, R, R, O,
-    R, R, R, R, R, R, R, R,
-    R, R, R, R, R, R, R, R,
-    O, R, R, R, R, R, R, O,
-    O, O, R, R, R, R, O, O,
-    ]
-heart = [
-    O, O, O, O, O, O, O, O,
-    O, P, P, O, P, P, O, O,
-    P, P, P, P, P, P, P, O,
-    P, P, P, P, P, P, P, O,
-    O, P, P, P, P, P, O, O,
-    O, O, P, P, P, O, O, O,
-    O, O, O, P, O, O, O, O,
-    O, O, O, O, O, O, O, O,
-    ]
-question_mark = [
-O, O, O, R, R, O, O, O,
-O, O, R, O, O, R, O, O,
-O, O, O, O, O, R, O, O,
-O, O, O, O, R, O, O, O,
-O, O, O, R, O, O, O, O,
-O, O, O, R, O, O, O, O,
-O, O, O, O, O, O, O, O,
-O, O, O, R, O, O, O, O
-]
 
-image = [image_pixels, raspi, heart, question_mark]
+def image_pixels(X):
+    image = [O, O, O, O, O, O, O, O,
+             O, O, O, X, O, O, O, O,
+             O, O, X, X, X, O, O, O,
+             O, X, O, X, O, X, O, O,
+             O, O, O, X, O, O, O, O,
+             O, O, O, X, O, O, O, O,
+             O, O, O, X, O, O, O, O, 
+             O, O, O, O, O, O, O, O]
+    return image
+    
+def heart(X):
+    img = [
+        O, O, O, O, O, O, O, O,
+        O, X, X, O, X, X, O, O,
+        X, X, X, X, X, X, X, O,
+        X, X, X, X, X, X, X, O,
+        O, X, X, X, X, X, O, O,
+        O, O, X, X, X, O, O, O,
+        O, O, O, X, O, O, O, O,
+        O, O, O, O, O, O, O, O,]
+    return img
+
+def question_mark(X):
+    img = [
+        O, O, O, X, X, O, O, O,
+        O, O, X, O, O, X, O, O,
+        O, O, O, O, O, X, O, O,
+        O, O, O, O, X, O, O, O,
+        O, O, O, X, O, O, O, O,
+        O, O, O, X, O, O, O, O,
+        O, O, O, O, O, O, O, O,
+        O, O, O, X, O, O, O, O]
+    return img
+
+def clear():
+  for i in range(8):
+    for j in range(8):
+      sense.set_pixel(i, j, (0, 0, 0))
+      
+colors = [G, Y, B, R, P]
+image = [image_pixels, heart, question_mark]
 counter = 0
+
 while(True):
-    sense.set_pixels(image[counter % 4])
+    clear()
+    sense.set_rotation(90*randint(0, 3))
+    sense.set_pixels(image[counter % 3](colors[randint(0, 4)]))
     time.sleep(1)
     counter += 1
