b83e4bba48c6ca1f98798d832b7e51e18a4b25e8
Danxu Zhang <dzhang022@e.ntu.edu.sg>
2021-09-20 11:21:29 +0800

    Update tut - add class content

--- Week7/Tut.py
+++ Week7/Tut.py
@@ -19,26 +19,46 @@ def reverseAndOppositeCase(str):
     if (len(str) == 1):
         if (str.islower()):
             return str.upper()
-        else:
+        elif (str.isupper()):
             return str.lower()
+        else: # Consider about special chars
+            return str
     else:
         if (str[0].islower()):
             new_str = reverseAndOppositeCase(str[1:]) + str[0].upper()
-        else:
+        elif (str[0].isupper()):
             new_str = reverseAndOppositeCase(str[1:]) + str[0].lower()
+        else:
+            new_str = reverseAndOppositeCase(str[1:]) + str[0]
         return new_str
 
 # Discussion 3
 def symmetricString(str):
-    rev_str = reverse(str)
-    new_str = str + rev_str
-    return new_str 
+    # rev_str = reverse(str)
+    # new_str = str + rev_str
+    # return new_str
+    if (len(str) == 1):
+        return str * 2
+    else:
+        new_str = str[0] + symmetricString(str[1:]) + str[0]
+        return new_str
+     
 
-
+# Additional: check is a string is palidrome(symmatric)
+def is_symmetric(str):
+    if (len(str) == 1 or len(str) == 0):
+        return True
+    else:
+        if str[0] != str[-1]:
+            return False
+        else:
+            return is_symmetric(str[1:-1])
+     
 ## test ##
 
 # myStr1 = "ABC"
 # myStr2 = "AbCdE"
+# palindrome = "dogmaiamgod"
 
 # # Discussion1:
 # print(reverseAndRepeat(myStr1, 2))
@@ -49,3 +69,7 @@ def symmetricString(str):
 
 # # Discussion3:
 # print(symmetricString(myStr2))
+
+# # Additional:
+# print(is_symmetric(palindrome))
+# print(is_symmetric(symmetricString("Singapore")))
